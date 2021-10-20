bc54a0180ad51e3ff0c2a5ab519c3dd4fc882bf9
Vlad <vlad.47304aaaa@mail.ru>
2021-10-20 19:24:37 +0300

    add dbcontext

---
+++ DataAccess/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using DataAccess.Models;
+
+namespace DataAccess
+{
+    class ApplicationContext : DbContext
+    {
+        public ApplicationContext() 
+            : base(@"data source=DESKTOP-D1BVDQU;Initial Catalog=MoneyAccounting;Integrated Security=True;")
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ DataAccess/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public int Balance { get; set; }
+    }
+}
