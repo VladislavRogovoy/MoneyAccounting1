ed492a56f6e6da2b94100ce7224b2f456c1d4790
Vlad <vlad.47304aaaa@mail.ru>
2021-10-25 19:17:49 +0300

    transaction history tests

--- UnitTests/RepositoryTests.cs
+++ UnitTests/RepositoryTests.cs
@@ -12,12 +12,14 @@ namespace UnitTests
     {
         private string _contectionString;
         private IRepository<Category> _categoryRepository;
+        private IRepository<TransactionsHistory> _tHistory;
 
         [SetUp]
         public void Init()
         {
             //_contectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
             _categoryRepository = new CategoryRepository(new ApplicationContext());
+            _tHistory = new TransctionsRepository(new ApplicationContext());
         }
 
         [Test]
@@ -110,6 +112,99 @@ namespace UnitTests
             Assert.AreEqual(category.Balance, foundCategory.Balance);
 
             _categoryRepository.Delete(category.Id);
+            _categoryRepository.Save();
+        }
+
+        [Test]
+        public void AddNewTransactionsHistory()
+        {
+            //arrange
+            var tHistory = new TransactionsHistory()
+            {
+                CategoryId = 2,
+                History = "test history"
+            };
+
+            //act
+            _tHistory.Add(tHistory);
+            _tHistory.Save();
+
+            // Assert
+            Assert.NotZero(tHistory.Id);
+            _tHistory.Delete(tHistory.Id);
+            _tHistory.Save();
+        }
+
+        [Test]
+        public void DeleteTransactionHistory_ShouldReturnNull()
+        {
+            // Arrange
+            var tHistory = new TransactionsHistory()
+            {
+                CategoryId = 2,
+                History = "test history"
+            };
+
+            // Act
+            _tHistory.Add(tHistory);
+            _tHistory.Save();
+            var tHistoryId = tHistory.Id;
+            _tHistory.Delete(tHistoryId);
+            _tHistory.Save();
+            var foundTHistory = _tHistory.Find(tHistoryId);
+            Assert.Null(foundTHistory);
+        }
+
+        [Test]
+        public void FindtHistory_ShouldFindCreatedTHistory()
+        {
+            // Arrange
+            var tHistory = new TransactionsHistory()
+            {
+                CategoryId = 2,
+                History = "test history"
+            };
+
+            //Act
+            _tHistory.Add(tHistory);
+            _tHistory.Save();
+
+            var foundtHistory = _tHistory.Find(tHistory.Id);
+
+            //Assert 
+            Assert.AreEqual(tHistory.Id, foundtHistory.Id);
+            Assert.AreEqual(tHistory.History, foundtHistory.History);
+
+            _tHistory.Delete(tHistory.Id);
+            _tHistory.Save();
+        }
+
+        [Test]
+        public void UpdateTHistory_ShouldFindUpdatedTHistory()
+        {
+            //Arrange
+            var tHistory = new TransactionsHistory()
+            {
+                CategoryId = 2,
+                History = "test history"
+            };
+
+            //Act
+            _tHistory.Add(tHistory);
+            _tHistory.Save();
+            tHistory.CategoryId = 3;
+            tHistory.History = "test 2";
+            _tHistory.Update(tHistory);
+            _tHistory.Save();
+
+            var foundTHistory = _tHistory.Find(tHistory.Id);
+
+            //Assert
+            Assert.AreEqual(tHistory.CategoryId, foundTHistory.CategoryId);
+            Assert.AreEqual(tHistory.History, foundTHistory.History);
+
+            _tHistory.Delete(tHistory.Id);
+            _tHistory.Save();
         }
     }
 }
