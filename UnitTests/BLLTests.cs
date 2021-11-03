b0c1ce0d2021d86ad11128fc9ee0e96e912dca72
Vlad <vlad.47304aaaa@mail.ru>
2021-11-03 20:26:45 +0300

    tests

---
+++ UnitTests/BLLTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using System.Configuration;
+using DataAccess.Temp.IRepos;
+using DataAccess;
+using DataAccess.Temp.Repositories;
+using DataAccess.Models;
+using BusinessLogicLayer.Managers;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class BLLTests
+    {
+        private IRepository<Category> _categoryRepository;
+        private CategoryManager categoryManager;
+
+        [SetUp]
+        public void Init()
+        {
+            var dbContext = new ApplicationContext(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            _categoryRepository = new CategoryRepository(dbContext);
+            categoryManager = new CategoryManager(_categoryRepository);
+        }
+
+        [Test]
+        public void CreateCategory_ShouldReturnNewCategory()
+        {
+            //Act
+            categoryManager.CreateNewCategory("TestCategory");
+
+            //Arrange
+            var allCategories = categoryManager.GetAllCategories();
+            var category = allCategories.FirstOrDefault();
+
+            //Assert
+            Assert.IsNotNull(category);
+            Assert.AreEqual(category.CategoryName, "TestCategory");
+            categoryManager.DeleteCategory(category.Id);
+        }
+    }
+}
--- UnitTests/RepositoryTests.cs
+++ UnitTests/RepositoryTests.cs
@@ -10,16 +10,15 @@ namespace UnitTests
     [TestFixture]
     public class RepositoryTests
     {
-        private string _contectionString;
         private IRepository<Category> _categoryRepository;
-        private IRepository<TransactionsHistory> _tHistory;
+        private IRepository<Transaction> _transactions;
 
         [SetUp]
         public void Init()
         {
-            //_contectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            _categoryRepository = new CategoryRepository(new ApplicationContext());
-            _tHistory = new TransctionsRepository(new ApplicationContext());
+            var dbContext = new ApplicationContext(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            _categoryRepository = new CategoryRepository(dbContext);
+            _transactions = new TransctionsRepository(dbContext);
         }
 
         [Test]
@@ -116,95 +115,105 @@ namespace UnitTests
         }
 
         [Test]
-        public void AddNewTransactionsHistory()
+        public void AddNewTransaction()
         {
             //arrange
-            var tHistory = new TransactionsHistory()
+            var trns = new Transaction()
             {
                 CategoryId = 2,
-                History = "test history"
+                IsAddition = true,
+                Amount = 15
             };
 
             //act
-            _tHistory.Add(tHistory);
-            _tHistory.Save();
-
-            // Assert
-            Assert.NotZero(tHistory.Id);
-            _tHistory.Delete(tHistory.Id);
-            _tHistory.Save();
-        }
-
-        [Test]
-        public void DeleteTransactionHistory_ShouldReturnNull()
-        {
-            // Arrange
-            var tHistory = new TransactionsHistory()
-            {
-                CategoryId = 2,
-                History = "test history"
-            };
-
-            // Act
-            _tHistory.Add(tHistory);
-            _tHistory.Save();
-            var tHistoryId = tHistory.Id;
-            _tHistory.Delete(tHistoryId);
-            _tHistory.Save();
-            var foundTHistory = _tHistory.Find(tHistoryId);
-            Assert.Null(foundTHistory);
-        }
-
-        [Test]
-        public void FindtHistory_ShouldFindCreatedTHistory()
-        {
-            // Arrange
-            var tHistory = new TransactionsHistory()
-            {
-                CategoryId = 2,
-                History = "test history"
-            };
-
-            //Act
-            _tHistory.Add(tHistory);
-            _tHistory.Save();
-
-            var foundtHistory = _tHistory.Find(tHistory.Id);
-
-            //Assert 
-            Assert.AreEqual(tHistory.Id, foundtHistory.Id);
-            Assert.AreEqual(tHistory.History, foundtHistory.History);
-
-            _tHistory.Delete(tHistory.Id);
-            _tHistory.Save();
-        }
-
-        [Test]
-        public void UpdateTHistory_ShouldFindUpdatedTHistory()
-        {
-            //Arrange
-            var tHistory = new TransactionsHistory()
-            {
-                CategoryId = 2,
-                History = "test history"
-            };
-
-            //Act
-            _tHistory.Add(tHistory);
-            _tHistory.Save();
-            tHistory.CategoryId = 3;
-            tHistory.History = "test 2";
-            _tHistory.Update(tHistory);
-            _tHistory.Save();
-
-            var foundTHistory = _tHistory.Find(tHistory.Id);
+            _transactions.Add(trns);
+            _transactions.Save();
 
             //Assert
-            Assert.AreEqual(tHistory.CategoryId, foundTHistory.CategoryId);
-            Assert.AreEqual(tHistory.History, foundTHistory.History);
+            Assert.NotZero(trns.Id);
+            _transactions.Delete(trns.Id);
+            _transactions.Save();
+        }
 
-            _tHistory.Delete(tHistory.Id);
-            _tHistory.Save();
+        [Test]
+        public void DeleteTransaction_ShouldReturnNull()
+        {
+            //Arrange
+            var trns = new Transaction()
+            {
+                CategoryId = 2,
+                IsAddition = true,
+                Amount = 15
+            };
+
+            //Act
+            _transactions.Add(trns);
+            _transactions.Save();
+
+            var trnsId = trns.Id;
+            _transactions.Delete(trnsId);
+            _transactions.Save();
+            var foundTrns = _transactions.Find(trnsId);
+            Assert.Null(foundTrns);
+        }
+
+        [Test]
+        public void FindTransaction_ShouldFindCreatedTransaction()
+        {
+            //Arrange
+            var trns = new Transaction()
+            {
+                CategoryId = 2,
+                IsAddition = true,
+                Amount = 15
+            };
+
+            //Act
+            _transactions.Add(trns);
+            _transactions.Save();
+
+            var foundTransaction = _transactions.Find(trns.Id);
+
+            //arrange
+            Assert.AreEqual(trns.Id, foundTransaction.Id);
+            Assert.AreEqual(trns.IsAddition, foundTransaction.IsAddition);
+            Assert.AreEqual(trns.CategoryId, foundTransaction.CategoryId);
+            Assert.AreEqual(trns.Amount, foundTransaction.Amount);
+
+            _transactions.Delete(trns.Id);
+            _transactions.Save();
+        }
+
+        [Test]
+        public void UpdateTransaction_ShouldReturnUpdatedTransaction()
+        {
+            //Act
+            var trns = new Transaction()
+            {
+                CategoryId = 2,
+                IsAddition = true,
+                Amount = 15
+            };
+
+            //Arrange
+            _transactions.Add(trns);
+            _transactions.Save();
+
+            trns.CategoryId = 3;
+            trns.IsAddition = false;
+            trns.Amount = 10;
+            _transactions.Update(trns);
+            _transactions.Save();
+
+            var foundTrns = _transactions.Find(trns.Id);
+
+            //Assert
+            Assert.AreEqual(trns.CategoryId, foundTrns.CategoryId);
+            Assert.AreEqual(trns.Amount, foundTrns.Amount);
+            Assert.AreEqual(trns.IsAddition, foundTrns.IsAddition);
+
+            _transactions.Delete(trns.Id);
+            _transactions.Save();
         }
     }
 }
